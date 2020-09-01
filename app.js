f2138272181f3d74cc51080ac27c10a75fec55ba
jeremy <jeremy1910@gmail.com>
2020-09-01 22:33:36 +0200

    WIP

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const { getProg } = require('./src/modules/scraper');
 
 (async () => {
-    console.log(await getProg('2020-08-31'))
+    console.log(await getProg('2020-09-01'))
 })()
 
 
--- src/modules/scraper.js
+++ src/modules/scraper.js
@@ -9,26 +9,50 @@ const getPage = async (day) => {
     return data
 }
 
+const getSynopsis = async (link) => {
+    try {
+        const { data } = await axios.get(link)
+        const $ = cheerio.load(data)
+        const description = $('div.synopsis-text.defaultStyleContentTags').text().trim()
+        return description || null
+    } catch (e) {
+        console.log('error', link)
+        return null
+    }
+   
+}
+
 
 const parsePage = (data) => {
     const $ = cheerio.load(data)
     const canals = $('div.bouquet-epgGrid').find('div.doubleBroadcastCard')
     
-    canals.each( (index, canal) => {
+    canals.each( async (index, canal) => {
         const heure = $(canal).find('.doubleBroadcastCard-hour')
         const nom =  $(canal).find('.doubleBroadcastCard-title')
         const channel = $(canal).find('.doubleBroadcastCard-channelName')
+        const type = $(canal).find('.doubleBroadcastCard-type')
+        const duration = $(canal).find('span.doubleBroadcastCard-durationContent')
+        const descriptions = [
+            await getSynopsis($(nom[0]).attr('href').trim()),
+            await getSynopsis($(nom[1]).attr('href').trim())
+        ]
         const shedule = {
             chaine: $(channel).text().trim(),
             shedules: [
                 {
                     heure: $(heure[0]).text().trim(),
-                    programme: $(nom[0]).text().trim()
+                    programme: $(nom[0]).text().trim(),
+                    description: descriptions[0],
+                    type: $(type[0]).text().trim(),
+                    duration: $(duration[0]).text().trim()
                 },
                 {
                     heure: $(heure[1]).text().trim(),
-                    programme: $(nom[1]).text().trim()
-                }
+                    programme: $(nom[1]).text().trim(),
+                    description: descriptions[1],
+                    type: $(type[1]).text().trim(),
+                    duration: $(duration[1]).text().trim()                }
             ]
         }
         console.log(shedule)

c426584da24f3b580e4d3e5b39e0128484d39ecb
jeremy <jeremy1910@gmail.com>
2020-09-02 22:41:12 +0200

    WIP

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 require('dotenv').config()
+const db = require('monk')('localhost/progTv')
+const dbChannels = db.get('channels')
 
+const { GraphQLServer } = require('graphql-yoga')
 
 const { getProg } = require('./src/modules/scraper');
 
+/* getProg('2020-09-01') */
+
 (async () => {
-    console.log(await getProg('2020-09-01'))
+    console.log(JSON.stringify(await dbChannels.find({ channel: 'TF1' }, {schedules: {schedule: {hour: 1}, day: 1} })))
 })()
 
+const typeDefs = `
+  type Channel {
+    channel: String
+  }
+
+  type Schedule {
+      day: String,
+      hour: String
+  }
+
+  type Schedules {
+      channel: String,
+      schedules: [Schedule] 
+  }
+
+  type Query {
+    channelList: [Channel],
+    schedulesForChannel(channel: String): [Schedules]
+  }
+`
+ 
+const resolvers = {
+  Query: {
+    channelList: async (_, {}) => await dbChannels.find({}, {channel: 1}),
+    schedulesForChannel: async(_, { channel }) => await dbChannels.find({ channel }, {schedules: {schedule: {hour: 1}, day: 1} })
+  },
+}
 
 
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+server.start(() => console.log('Server is running on localhost:4000'))
--- src/modules/scraper.js
+++ src/modules/scraper.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio')
 const axios = require('axios')
-
+const db = require('monk')('localhost/progTv')
+const dbChannels = db.get('channels')
 
 const baseURL = process.env.URL_SCRAPING_BASE
 
@@ -22,14 +23,14 @@ const getSynopsis = async (link) => {
 }
 
 
-const parsePage = (data) => {
+const parsePage = async (data) => {
     const schedules = []
 
     const $ = cheerio.load(data)
-    const canals = $('div.bouquet-epgGrid').find('div.doubleBroadcastCard')
+    const canals = $('div.bouquet-epgGrid').find('div.doubleBroadcastCard').toArray()
+    console.log(canals.length)
 
-
-     canals.each( async (index, canal) => {
+     await Promise.all(canals.map( async (canal) => {
         const heure = $(canal).find('.doubleBroadcastCard-hour')
         const nom =  $(canal).find('.doubleBroadcastCard-title')
         const channel = $(canal).find('.doubleBroadcastCard-channelName')
@@ -57,17 +58,46 @@ const parsePage = (data) => {
 
 
 
-    })
+    }))
     return schedules
 }
 
 
+const persistInDb = async (channel, day) => {
+    const newSchedule = {
+        day: new Date(day),
+        schedule: channel.channelSchedule
+    }
+
+    const foundedChannel = await dbChannels.findOne({channel: channel.channel})
+    if(foundedChannel) {
+        console.log(`chaine ${channel.channel} Trouvé`)
+        if(!foundedChannel.schedules.find( schedule => new Date(schedule.day).toDateString() === new Date(day).toDateString())){
+            console.log(`chaine ${channel.channel}, jour ${day} n'existe pas`)
+            foundedChannel.schedules.push(newSchedule)
+            dbChannels.update({_id: foundedChannel._id}, {$set: {schedules: foundedChannel.schedules}})
+        }
+    } else {
+        console.log(`chaine ${channel.channel} non trouvé`)
+        dbChannels.insert({
+            channel: channel.channel,
+            schedules: [
+                newSchedule
+            ]
+        })
+    }
+    console.log(`chaine ${channel.channel} writted`)
+    return null
+}
+
 const getProg = async (day) => {
     const data = await getPage(day)
     if(data) {
-        const schedules =  parsePage(data)
-        console.log(schedules)
+        const channels = await parsePage(data)
+        console.log('nombre de chaines : ', channels.length)
+        await Promise.all(channels.map(async (channel) => await persistInDb(channel, day)))
     }
+    db.close()
 }
 
 module.exports = { getProg }
